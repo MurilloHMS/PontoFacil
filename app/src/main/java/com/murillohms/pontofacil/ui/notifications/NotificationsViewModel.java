c7c3d20c6c6c8097c931ed0f2bce2c00e7fda5cb
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-04 22:57:50 -0300

    feat: include notifications fragment and view model

--- app/src/main/java/com/murillohms/pontofacil/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/murillohms/pontofacil/ui/notifications/NotificationsViewModel.java
@@ -1,19 +1,39 @@
 package com.murillohms.pontofacil.ui.notifications;
 
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+import com.murillohms.pontofacil.domain.entity.FuncionarioEntity;
+import com.murillohms.pontofacil.infrastructure.repository.PontoRepository;
 
-    private final MutableLiveData<String> mText;
+public class NotificationsViewModel extends AndroidViewModel {
 
-    public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+    private final PontoRepository repository;
+    private final LiveData<FuncionarioEntity> funcionarioAtivo;
+    private final MutableLiveData<Integer> totalRegistros = new MutableLiveData<>();
+
+    public NotificationsViewModel(@NonNull Application application) {
+        super(application);
+        repository = new PontoRepository(application);
+        funcionarioAtivo = repository.getFuncionarioAtivo();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void limparHistorico(int funcionarioId) {
+        repository.limparRegistros(funcionarioId);
+    }
+
+    public PontoRepository getRepository() {
+        return repository;
+    }
+
+    public LiveData<FuncionarioEntity> getFuncionarioAtivo() {
+        return funcionarioAtivo;
+    }
+
+    public LiveData<Integer> getTotalRegistros(int funcionarioId) {
+        return repository.getCountRegistros(funcionarioId);
     }
 }
